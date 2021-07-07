4dabde8843edff4961b6daed47eb999722b151bc
Sam Lloyd <bobdoe784@gmail.com>
2021-07-07 14:54:26 -0700

    Prompts for basic questions added to terminal

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // TODO: Include packages needed for this application
-
+const questions = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+ questions
+ .prompt([
+     {
+         type:'input',
+         message:'What is your username on Github?',
+         name: 'username',
+
+     },
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Describe the purpose of your project",
+      name: 'description'
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
